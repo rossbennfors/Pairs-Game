a7ec2c4d06f7e0a1aadf2a60dd20af87aef4596b
rossbennfors <110924918+rossbennfors@users.noreply.github.com>
2023-07-18 12:35:44 +0100

    Delete pairs directory

--- pairs/save_score_function.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$username = $_SESSION['username'];
-
-header('Content-type: application/json; charset=UTF-8');
-$scoreData = json_decode(file_get_contents("php://input"));
-$_SESSION['scoreData'] = json_encode($scoreData);
-echo json_encode($scoreData);
-?>
